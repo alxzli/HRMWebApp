680959104b28c4410f370145c902cf9cddd34b1b
alxzli <ac107119@avcol.school.nz>
2022-03-25 10:11:25 +1300

    tidying up

--- Hayatsu Ramen/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Hayatsu_Ramen.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
