ba5a92dd5bb07fd40230b0c6168b9e85642dc810
alxzli <ac107119@avcol.school.nz>
2022-03-25 10:07:02 +1300

    Version 0

--- Hayatsu Ramen/wwwroot/js/site.js
+++ Hayatsu Ramen/wwwroot/js/site.js
@@ -27,9 +27,9 @@ var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
 	var currentScrollPos = window.pageYOffset;
 	if (prevScrollpos > currentScrollPos) {
-		document.getElementById("mainNav").style.top = "0";
+		document.getElementById("navbar").style.top = "0";
 	} else {
-		document.getElementById("mainNav").style.top = "-80px";
+		document.getElementById("navbar").style.top = "-50px";
 	}
 	prevScrollpos = currentScrollPos;
 }
