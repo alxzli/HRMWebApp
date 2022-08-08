46f8cedfde2c21576870e113fd7d4a2548a12ce8
Alex Li <alxzli@icloud.com>
2022-08-08 15:07:55 +1200

    v1

---
+++ HRMWebApp/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMWebApp.Models
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+        [Required]
+        public int UnitNo { get; set; }
+        [Required]
+        public string Street { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public int Zip { get; set; }
+        [Required]
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; }
+    }
+}
---
+++ HRMWebApp/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HRMWebApp.Models
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int Phone { get; set; }
+        [Required]
+        public int AddressID { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
